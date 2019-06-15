25dc37e5774da8fea7e2a244f54d1f42e1f980a1
Amin Amos <aminamosahs@gmail.com>
2019-06-15 23:15:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,23 @@ def array_with_two_elements
   array = [1, 2]
   array
 end
+
+def first_element(arg)
+  arg[0]
+end
+
+def third_element(arg)
+  arg[2]
+end
+
+def last_element(arg)
+  arg[-1]
+end
+
+def first_element_with_array_methods
+end
+
+def 
+
+
+
