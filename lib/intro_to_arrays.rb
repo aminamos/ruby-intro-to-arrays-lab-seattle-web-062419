12709bd5d26ed5d73d0e0aefa04bd60028bca5ad
Amin Amos <aminamosahs@gmail.com>
2019-06-15 23:18:39 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  array = []
+  array
+end
+
+def array_with_two_elements
+  array = [1, 2]
+  array
+end
+
+def first_element(arg)
+  arg[0]
+end
+
+def third_element(arg)
+  arg[2]
+end
+
+def last_element(arg)
+  arg[-1]
+end
+
+def first_element_with_array_methods(arg)
+  arg.first
+end
+
+def last_element_with_array_methods(arg)
+  arg.last
+end
+
+def length_of_array(arg)
+  arg.length
+end
+
+
+
